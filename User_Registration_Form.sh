bf6ed476b2bb980678b01f129e2a364ee5c38025
Apoorva1217 <Apoorva1217@gmail.com>
2020-07-16 12:25:58 +0530

    pattern for valid email

--- User_Registration_Form.sh
+++ User_Registration_Form.sh
@@ -25,3 +25,16 @@ then
 else
         echo "Invalid"
 fi
+
+#enter a valid email
+echo "Enter the Email ID:"
+read email
+
+emailpatt="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+[.]+([a-zA-Z]{2,4})+[.]*([a-zA-Z]{2})*$"
+
+if [[ $email =~ $emailpatt ]]
+then
+        echo "Valid Email ID"
+else
+        echo "Invalid Email ID"
+fi
