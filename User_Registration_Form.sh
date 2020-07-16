74fe784db763213e09f5b7362f98611757ca735d
Apoorva1217 <Apoorva1217@gmail.com>
2020-07-16 12:24:39 +0530

    pattern for valid Last Name

--- User_Registration_Form.sh
+++ User_Registration_Form.sh
@@ -12,3 +12,16 @@ then
 else
        echo "Invalid"
 fi
+
+#enter a valid Last Name
+echo "Enter the Last Name:"
+read lname
+
+lnamepatt="^[A-Z]{1}[a-z]{2,}$"
+
+if [[ $lname =~ $lnamepatt ]]
+then
+        echo "Valid Lname"
+else
+        echo "Invalid"
+fi
